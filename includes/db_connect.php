0b3789c33530db7707d979315a19c10e17b17477
hardyJ01 <connect.hardip.com>
2025-10-03 01:02:17 +0530

    adding the .env variable

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$db = 'travel_circle_db';
-$user = 'root';
-$pass = 'Hardip#20';
+$env = parse_ini_file(__DIR__ . '/../.env');
+$host = $env['host'];
+$db = $env['db'];
+$user = $env['user'];
+$pass = $env['pass'];
 
 $conn = new mysqli($host,$user,$pass,$db);
 
 if($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
 }
-// echo "Connected successfully";
 ?>
